9ff153e7caeff1bbdddb12aab91e9c66fcaa2b36
skuodi <27884181+skuodi@users.noreply.github.com>
2024-05-03 13:03:44 +0300

    Imlemented the SBS protocol specific to TI's BQ30/BQ40 series with SHA1 authentication

---
+++ sbs_bq.c
@@ -0,0 +1,278 @@
+#include <stdint.h>
+#include <string.h>
+
+#include "libs/WjCryptLib/lib/WjCryptLib_Sha1.h"
+
+#include "platform/smbus_platform.h"
+#include "sbs_bq.h"
+
+inline int SBSBqManufacturerBlockAccess(smbus_handle_t handle, uint8_t devAddr, uint16_t subcommand,
+                                        uint8_t *dataRecv, uint8_t *dataLength, int delayMs)
+{
+  return SMBusWriteWordBlockReadBlock(handle, devAddr, SBS_BQ_COMMAND_MANUFACTURER_BLOCK_ACCESS, subcommand,
+                                      SBS_BQ_COMMAND_MANUFACTURER_BLOCK_ACCESS, dataRecv, dataLength, delayMs);
+}
+
+inline int SBSBqManufacturerAccess(smbus_handle_t handle, uint8_t devAddr, uint16_t subcommand,
+                                   uint8_t *dataRecv, uint8_t *dataLength, int delayMs)
+{
+  return SMBusWriteWordReadBlock(handle, devAddr, SBS_COMMAND_MANUFACTURER_ACCESS, subcommand, true,
+                                 SBS_COMMAND_MANUFACTURER_DATA, dataRecv, dataLength, delayMs);
+}
+
+int SBSBqAccessSha1Hmac(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint8_t *key)
+{
+  if (!handle || !key)
+    return SMBUS_ERR_INVALID_ARG;
+
+  uint8_t msgLen;
+  uint8_t dataBuff[256];
+  uint8_t flipBuff[20];
+
+  SHA1_HASH hash;
+  memset(dataBuff, 0, sizeof(dataBuff));
+
+  // Send the unseal command and receive a 20-byte challenge message
+  int ret = SBSBqManufacturerAccess(handle, devAddr, accessCmd, dataBuff + 16, &msgLen, 50);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+  if (msgLen != 20)
+    return SMBUS_ERR_UNEXPECTED_DATA_RECEIVED;
+
+  // Data is transferred LSByte first but hash is calculated MSByte first so reverse the order of received bytes
+  for (int i = 20; i > 0; i--)
+    flipBuff[sizeof(flipBuff) - i] = (dataBuff + 16)[i - 1];
+
+  // H1 = H(K + M)
+  memcpy(dataBuff, key, 16);
+  memcpy(dataBuff + 16, flipBuff, sizeof(flipBuff));
+
+#ifdef SBS_BQ_LOG
+  printf("B1 : ");
+  for (int i = 0; i < 36; i++)
+    printf("%2.2X", dataBuff[i]);
+  printf("\n");
+#endif
+
+  Sha1Calculate(dataBuff, 36, &hash);
+
+#ifdef SBS_BQ_LOG
+  printf("H1 : ");
+  for (int i = 0; i < sizeof(hash); i++)
+    printf("%2.2X", hash.bytes[i]);
+  printf("\n");
+#endif
+
+  // H2 = H(K + H1)
+  memcpy(dataBuff + 16, hash.bytes, 20);
+
+#ifdef SBS_BQ_LOG
+  printf("B2 : ");
+  for (int i = 0; i < 36; i++)
+    printf("%2.2X", dataBuff[i]);
+  printf("\n");
+#endif
+
+  Sha1Calculate(dataBuff, 36, &hash);
+
+#ifdef SBS_BQ_LOG
+  printf("H2 : ");
+  for (int i = 0; i < sizeof(hash); i++)
+    printf("%2.2X", hash.bytes[i]);
+  printf("\n");
+#endif
+
+  // Data is transferred LSByte first but hash is calculated MSByte first so reverse the order before sending
+
+  for (int i = 20; i > 0; i--)
+    flipBuff[sizeof(flipBuff) - i] = hash.bytes[i - 1];
+
+  ret = SMBusBlockWrite(handle, devAddr, SBS_COMMAND_OPTIONAL_MFG_FUNCTION5, hash.bytes, 20);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+
+  // Takes at least 250ms for the setting to take effect
+  SMBusPlatformDelayMs(500);
+
+  ret = SBSBqManufacturerAccess(handle, devAddr, SBS_BQ_COMMAND_OPERATION_STATUS, dataBuff, &msgLen, 50);
+  if (ret != SMBUS_ERR_OK || msgLen != 3)
+    return ret;
+
+  if (accessCmd == SBS_BQ_COMMAND_UNSEAL_DEVICE)
+    return ((dataBuff[1] & 1) && !(dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+  else if (accessCmd == SBS_BQ_COMMAND_FULL_ACCESS_DEVICE)
+    return (!(dataBuff[1] & 1) && (dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+
+  return SMBUS_ERR_FAIL;
+}
+
+int SBSBqBlockAccessSha1Hmac(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint8_t *key)
+{
+  if (!handle || !key)
+    return SMBUS_ERR_INVALID_ARG;
+
+  uint8_t msgLen;
+  uint8_t dataBuff[256];
+  uint8_t flipBuff[20];
+
+  SHA1_HASH hash;
+  memset(dataBuff, 0, sizeof(dataBuff));
+
+  // Send the unseal command and receive a 20-byte challenge message
+  int ret = SBSBqManufacturerBlockAccess(handle, devAddr, accessCmd, dataBuff + 16, &msgLen, 50);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+  if (msgLen != 20)
+    return SMBUS_ERR_UNEXPECTED_DATA_RECEIVED;
+
+  // Data is transferred LSByte first but hash is calculated MSByte first so reverse the order of received bytes
+  for (int i = 20; i > 0; i--)
+    flipBuff[sizeof(flipBuff) - i] = (dataBuff + 16)[i - 1];
+
+  // H1 = H(K + M)
+  memcpy(dataBuff, key, 16);
+  memcpy(dataBuff + 16, flipBuff, sizeof(flipBuff));
+
+#ifdef SBS_BQ_LOG
+  printf("B1 : ");
+  for (int i = 0; i < 36; i++)
+    printf("%2.2X", dataBuff[i]);
+  printf("\n");
+#endif
+
+  Sha1Calculate(dataBuff, 36, &hash);
+
+#ifdef SBS_BQ_LOG
+  printf("H1 : ");
+  for (int i = 0; i < sizeof(hash); i++)
+    printf("%2.2X", hash.bytes[i]);
+  printf("\n");
+#endif
+
+  // H2 = H(K + H1)
+  memcpy(dataBuff + 16, hash.bytes, 20);
+
+#ifdef SBS_BQ_LOG
+  printf("B2 : ");
+  for (int i = 0; i < 36; i++)
+    printf("%2.2X", dataBuff[i]);
+  printf("\n");
+#endif
+
+  Sha1Calculate(dataBuff, 36, &hash);
+
+#ifdef SBS_BQ_LOG
+  printf("H2 : ");
+  for (int i = 0; i < sizeof(hash); i++)
+    printf("%2.2X", hash.bytes[i]);
+  printf("\n");
+#endif
+
+  // Data is transferred LSByte first but hash is calculated MSByte first so reverse the order before sending
+
+  for (int i = 20; i > 0; i--)
+    flipBuff[sizeof(flipBuff) - i] = hash.bytes[i - 1];
+
+  ret = SMBusBlockWrite(handle, devAddr, SBS_COMMAND_OPTIONAL_MFG_FUNCTION5, hash.bytes, 20);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+
+  // Takes at least 250ms for the setting to take effect
+  SMBusPlatformDelayMs(500);
+
+  ret = SBSBqManufacturerBlockAccess(handle, devAddr, SBS_BQ_COMMAND_OPERATION_STATUS, dataBuff, &msgLen, 50);
+  if (ret != SMBUS_ERR_OK || msgLen != 3)
+    return ret;
+
+  if (accessCmd == SBS_BQ_COMMAND_UNSEAL_DEVICE)
+    return ((dataBuff[1] & 1) && !(dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+  else if (accessCmd == SBS_BQ_COMMAND_FULL_ACCESS_DEVICE)
+    return (!(dataBuff[1] & 1) && (dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+
+  return SMBUS_ERR_FAIL;
+}
+
+int SBSBqAccess2WordKey(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint16_t *key)
+{
+  if (!handle || !key)
+    return SMBUS_ERR_INVALID_ARG;
+
+  uint8_t recvCount;
+  uint8_t dataBuff[256];
+
+  int ret = SMBusWriteWord(handle, devAddr, SBS_COMMAND_MANUFACTURER_ACCESS, key[0]);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+
+  SMBusPlatformDelayMs(50);
+
+  ret = SMBusWriteWord(handle, devAddr, SBS_COMMAND_MANUFACTURER_ACCESS, key[1]);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+
+  // Takes at least 250ms for the setting to take effect
+  SMBusPlatformDelayMs(500);
+
+  ret = SBSBqManufacturerAccess(handle, devAddr, SBS_BQ_COMMAND_OPERATION_STATUS, dataBuff, &recvCount, 50);
+  if (ret != SMBUS_ERR_OK || recvCount != 3)
+    return ret;
+
+  if (accessCmd == SBS_BQ_COMMAND_UNSEAL_DEVICE)
+    return ((dataBuff[1] & 1) && !(dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+  else if (accessCmd == SBS_BQ_COMMAND_FULL_ACCESS_DEVICE)
+    return (!(dataBuff[1] & 1) && (dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+
+  return SMBUS_ERR_FAIL;
+}
+
+int SBSBqBlockAccess2WordKey(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint16_t *key)
+{
+  if (!handle || !key)
+    return SMBUS_ERR_INVALID_ARG;
+
+  uint8_t recvCount;
+  uint8_t dataBuff[256];
+
+  int ret = SMBusWriteWord(handle, devAddr, SBS_COMMAND_MANUFACTURER_ACCESS, key[0]);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+
+  SMBusPlatformDelayMs(50);
+
+  ret = SMBusWriteWord(handle, devAddr, SBS_COMMAND_MANUFACTURER_ACCESS, key[1]);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+
+  // Takes at least 250ms for the setting to take effect
+  SMBusPlatformDelayMs(500);
+
+  ret = SBSBqManufacturerBlockAccess(handle, devAddr, SBS_BQ_COMMAND_OPERATION_STATUS, dataBuff, &recvCount, 50);
+  if (ret != SMBUS_ERR_OK || recvCount != 3)
+    return ret;
+
+  if (accessCmd == SBS_BQ_COMMAND_UNSEAL_DEVICE)
+    return ((dataBuff[1] & 1) && !(dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+  else if (accessCmd == SBS_BQ_COMMAND_FULL_ACCESS_DEVICE)
+    return (!(dataBuff[1] & 1) && (dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+
+  return SMBUS_ERR_FAIL;
+}
+
+int SBSBqSeal(smbus_handle_t handle, uint8_t devAddr)
+{
+  uint8_t recvCount;
+  uint8_t dataBuff[256];
+
+  int ret = SMBusWriteWord(handle, devAddr, SBS_COMMAND_MANUFACTURER_ACCESS, SBS_BQ_COMMAND_SEAL_DEVICE);
+  if (ret != SMBUS_ERR_OK)
+    return ret;
+
+  // Takes at least 250ms for the setting to take effect
+  SMBusPlatformDelayMs(500);
+
+  ret = SBSBqManufacturerAccess(handle, devAddr, SBS_BQ_COMMAND_OPERATION_STATUS, dataBuff, &recvCount, 50);
+  if (ret != SMBUS_ERR_OK || recvCount != 3)
+    return ret;
+
+  return ((dataBuff[1] & 1) && (dataBuff[0] & 0x08)) ? SMBUS_ERR_OK : SMBUS_ERR_FAIL;
+}
---
+++ sbs_bq.h
@@ -0,0 +1,111 @@
+#ifndef _SBS_BQ_H_
+#define _SBS_BQ_H_
+
+/**
+ *
+ * The default UNSEAL key is 0x0414 and 0x3672. The default FULL ACCESS key is 0xFFFF and 0xFFFF.
+ * The default Manual PF key is 0x2857 and 0x2A98. The default Lifetimes Reset key is 0x2B14 and
+ * 0x2C8A.
+ * It is highly recommended to change the UNSEAL, FULL ACCESS, Manual PF, and Lifetimes Reset keys
+ * from default.
+ * The keys can only be changed through the ManufacturerBlockAccess().
+ * Example: Change UNSEAL key to 0x1234, 0x5678, and leave the other security keys at their default
+ * values.
+ * Send an SMBus block write with Command = 0x0035.
+ * Data = MAC command + UNSEAL key + FULL ACCESS KEY + PF key + Lifetimes Reset key
+ * = 35 00 34 12 78 56 FF FF FF FF 57 28 98 2A 14 2B 8A 2C
+ * NOTE: The first word of the keys cannot be the same. That means an UNSEAL key with 0xABCD
+ * 0x1234 and FULL ACCESS key with 0xABCD 0x5678 are not valid because the first word is
+ * the same.
+ * This is because the first word is used as a “detection” for the right command. This also
+ * means the first word cannot be the same as any existing MAC command.
+
+*/
+
+#define SBS_BQ_COMMAND_MANUFACTURER_BLOCK_ACCESS                        0x44 
+
+#define SBS_BQ_COMMAND_MANUFACTURER_DATA                                0x0000 
+#define SBS_BQ_COMMAND_DEVICE_TYPE                                      0x0001 
+#define SBS_BQ_COMMAND_FIRMWARE_VERSION                                 0x0002 
+#define SBS_BQ_COMMAND_HARDWARE_VERSION                                 0x0003 
+#define SBS_BQ_COMMAND_INSTRUCTION_FLASH_CHECKSUM                       0x0004
+#define SBS_BQ_COMMAND_DATA_FLASH_CHECKSUM                              0x0005
+#define SBS_BQ_COMMAND_CHEMICAL_ID                                      0x0006 
+#define SBS_BQ_COMMAND_SHUTDOWN_MODE                                    0x0010 
+#define SBS_BQ_COMMAND_SLEEP_MODE                                       0x0011 
+#define SBS_BQ_COMMAND_DEVICE_RESET                                     0x0012 
+#define SBS_BQ_COMMAND_FUSE_TOGGLE                                      0x001D 
+#define SBS_BQ_COMMAND_PRECHG_FET                                       0x001E 
+#define SBS_BQ_COMMAND_CHG_FET                                          0x001F 
+#define SBS_BQ_COMMAND_DSG_FET                                          0x0020 
+#define SBS_BQ_COMMAND_GAUGING                                          0x0021 
+#define SBS_BQ_COMMAND_FET_CONTROL                                      0x0022 
+#define SBS_BQ_COMMAND_LIFETIME_DATA_COLLECTION                         0x0023 
+#define SBS_BQ_COMMAND_PERMANENT_FAILURE                                0x0024 
+#define SBS_BQ_COMMAND_BLACK_BOX_RECORDER                               0x0025 
+#define SBS_BQ_COMMAND_FUSE                                             0x0026 
+#define SBS_BQ_COMMAND_LIFETIME_DATA_RESET                              0x0028 
+#define SBS_BQ_COMMAND_PERMANENT_FAIL_DATA_RESET                        0x0029 
+#define SBS_BQ_COMMAND_BLACK_BOX_RECORDER_RESET                         0x002A 
+#define SBS_BQ_COMMAND_CAL_MODE                                         0x002D 
+#define SBS_BQ_COMMAND_SEAL_DEVICE                                      0x0030 
+#define SBS_BQ_COMMAND_UNSEAL_DEVICE                                    0x0031 
+#define SBS_BQ_COMMAND_FULL_ACCESS_DEVICE                               0x0032 
+#define SBS_BQ_COMMAND_ROM_MODE                                         0x0033 
+#define SBS_BQ_COMMAND_UNSEAL_KEY                                       0x0035 
+#define SBS_BQ_COMMAND_FULL_ACCESS_KEY                                  0x0036 
+#define SBS_BQ_COMMAND_AUTHENTICATION_KEY                               0x0037 
+#define SBS_BQ_COMMAND_SAFETY_ALERT                                     0x0050 
+#define SBS_BQ_COMMAND_SAFETY_STATUS                                    0x0051 
+#define SBS_BQ_COMMAND_PF_ALERT                                         0x0052 
+#define SBS_BQ_COMMAND_PF_STATUS                                        0x0053 
+#define SBS_BQ_COMMAND_OPERATION_STATUS                                 0x0054 
+#define SBS_BQ_COMMAND_CHARGING_STATUS                                  0x0055 
+#define SBS_BQ_COMMAND_GAUGING_STATUS                                   0x0056 
+#define SBS_BQ_COMMAND_MANUFACTURING_STATUS                             0x0057 
+#define SBS_BQ_COMMAND_AFE_REGISTER                                     0x0058 
+#define SBS_BQ_COMMAND_TURBO_POWER                                      0x0059 
+#define SBS_BQ_COMMAND_TURBO_FINAL                                      0x005A 
+#define SBS_BQ_COMMAND_TURBO_PACK_R                                     0x005B 
+#define SBS_BQ_COMMAND_TURBO_SYS_R                                      0x005C 
+#define SBS_BQ_COMMAND_MIN_SYS_V                                        0x005D 
+#define SBS_BQ_COMMAND_TURBO_CURRENT                                    0x005E 
+#define SBS_BQ_COMMAND_LIFETIME_DATA_BLOCK_1                            0x0060 
+#define SBS_BQ_COMMAND_LIFETIME_DATA_BLOCK_2                            0x0061 
+#define SBS_BQ_COMMAND_LIFETIME_DATA_BLOCK_3                            0x0062 
+#define SBS_BQ_COMMAND_MANUFACTURER_INFO                                0x0070 
+#define SBS_BQ_COMMAND_VOLTAGES                                         0x0071 
+#define SBS_BQ_COMMAND_TEMPERATURES                                     0x0072 
+#define SBS_BQ_COMMAND_IT_STATUS1                                       0x0073 
+#define SBS_BQ_COMMAND_IT_STATUS2                                       0x0074 
+#define SBS_BQ_COMMAND_MANUAL FET CONTROL                               0x270C 
+#define SBS_BQ_COMMAND_EXIT_CALIBRATION_OUTPUT_MODE                     0xF080 
+#define SBS_BQ_COMMAND_OUTPUT_CC_AND_ADC_FOR_CALIBRATION                0xF081 
+#define SBS_BQ_COMMAND_OUTPUT_SHORTED_CC_AND_ADC_OFFSET_FOR_CALIBRATION 0xF082 
+#define SBS_BQ_COMMAND_DF_ACCESS_ROW_ADDRESS(r)                         (0x0100 | (r & 0b11))
+
+#include <stdint.h>
+#include <string.h>
+
+#include "libs/WjCryptLib/lib/WjCryptLib_Sha1.h"
+
+#include "sbs_smb.h"
+#include "sbs_bq.h"
+
+int SBSBqManufacturerBlockAccess(smbus_handle_t handle, uint8_t devAddr, uint16_t subcommand,
+                                        uint8_t *dataRecv, uint8_t *dataLength, int delayMs);
+
+int SBSBqManufacturerAccess(smbus_handle_t handle, uint8_t devAddr, uint16_t subcommand,
+                                   uint8_t *dataRecv, uint8_t *dataLength, int delayMs);
+
+int SBSBqAccessSha1Hmac(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint8_t *key);
+
+int SBSBqBlockAccessSha1Hmac(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint8_t *key);
+
+int SBSBqAccess2WordKey(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint16_t *key);
+
+int SBSBqBlockAccess2WordKey(smbus_handle_t handle, uint8_t devAddr, uint8_t accessCmd, uint16_t *key);
+
+int SBSBqSeal(smbus_handle_t handle, uint8_t devAddr);
+
+#endif
